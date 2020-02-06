78f05d547731a85538acdf4a6e326856a35636d4
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-02-05 19:38:40 -0600

    Finished the initial EditorsController

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -5,15 +5,29 @@ class EditorsController < ApplicationController
     render json: Editors.all(sort: { 'Name': 'asc'})
   end
   
-  # GET /editors/<email>
+  # GET /editors/email
   def get_by_email
+    render json: Editors.find_by_email(params[:email])
   end
 
   # POST /editors
   def create
+    if params[:name] && params[:email]
+      new_editor = Editors.create('Name': params[:name], 'Email': params[:email])
+      render json: new_editor 
+    elsif !params[:name]
+      render json: { 'error': 'A name is required to create an editor.' }
+    else
+      render json: { 'error': 'A email is required to create an editor.'}
+    end
   end
 
   # PUT /editors
   def edit 
+    editor = Editors.find(params[:id])
+    editor['Name'] = params[:name] if params[:name]
+    editor['Email'] = params[:email] if params[:email]
+    editor.save
+    render json: editor
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/editors', to: 'editors#index'
+  get '/editors/email', to: 'editors#get_by_email'
+  post '/editors', to: 'editors#create'
+  put '/editors', to: 'editors#edit'
 end
