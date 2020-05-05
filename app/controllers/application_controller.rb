8c2ba8b46d2597caba825b00f9f1b7a0826748b8
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-05-05 14:03:22 -0500

    Adding a before_action to validate a given Google oauth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_token
+  attr_accessor :email
+
+  private
+    def authenticate_token
+      validator = GoogleIDToken::Validator.new
+      payload = validator.check(parse_bearer_token, ENV['GOOGLE_CLIENT_ID'])
+      email = payload['email']
+      rescue GoogleIDToken::ValidationError => e
+        render json: { 'error': 'Invalid Google oauth token' }, status: 401
+    end
+
+    def parse_bearer_token
+      pattern = /^Bearer /
+      header  = request.env['Authorization'] 
+      header.gsub(pattern, '') if header && header.match(pattern)
+    end
 end
