5a04a8d4e1ca22e97352ee1faca15786d11037b6
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-06-21 13:57:21 -0500

    Wrote some tests for the application controller

--- test/controllers/base_controller_test.rb
+++ test/controllers/base_controller_test.rb
@@ -7,7 +7,7 @@ class BaseControllerTest < ActionDispatch::IntegrationTest
     def setup_google_auth_mocks
       ENV['GOOGLE_CLIENT_ID'] = 'client-id'
       @auth_editor = Editors.create('Name': 'auth', 'Email': 'auth@gmail.com')
-      GoogleIDToken::Validator.any_instance.expects(:check).with(AUTH_TOKEN, 'client-id').returns({ "email" => 'auth@gmail.com' })
+      GoogleIDToken::Validator.any_instance.expects(:check).with(AUTH_TOKEN, 'client-id').returns({ 'email' => 'auth@gmail.com' })
     end
 
     def delete_auth_editor
