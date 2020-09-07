427fb463981d03a4a588fa755a25aaffd0aa2d9a
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-09-07 08:40:19 -0500

    Commenting out the file_watcher setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,8 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
   config.airtable_editors_table_name = 'EditorsDevelopment'
   config.repo_name = 'msoe-sg/sg-editor-test-repo'
 end
